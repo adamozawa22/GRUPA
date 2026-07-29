35ee064768ffad4a0ee9f0647ecf6e2e59c47c30
adamozawa22 <adamekkolanek46@gmail.com>
2026-07-29 23:09:35 +0200

    Update Supabase configuration with new project details

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 // ⚠️ UZUPEŁNIJ TO PO ZAŁOŻENIU DARMOWEGO PROJEKTU NA supabase.com
 // Project Settings -> API -> "Project URL" oraz "anon public" key
-window.SUPABASE_URL = "https://TWOJ-PROJEKT.supabase.co";
-window.SUPABASE_ANON_KEY = "TWOJ_ANON_KEY";
+window.SUPABASE_URL = "https://mghhtqeckfrxlwtaeckh.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_secret_3G6EDEDF5mlhYCIvb5gIyg_9G9igQYN";
